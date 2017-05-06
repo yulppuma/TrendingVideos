598042881989dcbd7f2b1a2e89003d45d0c54d15
yulppuma <yulppuma@gmail.com>
2017-05-05 17:40:43 -0700

    Created methods to parse dates and get average value of columns, makes everything look purty

--- Trending_Vids_KNN/trending_vids.py
+++ Trending_Vids_KNN/trending_vids.py
@@ -3,47 +3,51 @@ import numpy as np
 from sklearn.neighbors import NearestNeighbors
 import math
 import os
+def getAverageRecent(column):
+    # Getting average of recent views from all 10000+ videos
+    test_recent_total = 0;
+    for i in range(len(column)):
+        test_recent_total += column[i]
+    test_recent_total /= (len(column) - 1)
+    return test_recent_total
 
+def parseDates(column):
+    # Initializing earliest and latest posted video
+    for i in range(len(column)):
+        str = column[i]
+        index = column[i].find("/", 3)
+        num = int(str[index + 1:])
+        column[i] = num
+    return column
 test_cols = ['user id', 'date_posted', 'recent_views', 'total_views']
 test = pd.read_csv('../data/test_trending (3).csv', sep=",", names=test_cols, encoding='latin-1')
 train_cols = ['user id', 'date_posted', 'recent_views', 'total_views']
 train = pd.read_csv('../data/train_trending(3).csv', sep=",", names=train_cols, encoding='latin-1')
-#Set columns of recent views and date poated to col_recent_test & col_date_test, respectively
+#Set columns of recent views and date posted to col_recent_test & col_date_test, respectively
 col_recent_test = np.array(test.recent_views)
 col_date_test = np.array(test.date_posted)
-#Getting average of recent views from all 10000+ videos
-test_recent_total = 0;
-#Don't know why I had this, but I think I want to find average of posted video dates
-test_date_total = 0
-#Initializing earliest and latest posted video
-earliest_date_test = None
-latest_date_test = None
-for i in range(len(col_recent_test)):
-    test_recent_total += col_recent_test[i]
-    str = col_date_test[i]
-    index = col_date_test[i].find("/", 3)
-    num = int(str[index + 1:])
-    col_date_test[i] = num
-    if i == 0:
-        earliest_date_test = num
-        latest_date_test = num
-    test_date_total += num
-    if num < earliest_date_test:
-        earliest_date_test = num
-    if num > latest_date_test:
-        latest_date_test = num
-test_recent_total/=(len(col_recent_test)-1)
-print(test_recent_total)
-print(col_date_test[0])
-print(earliest_date_test)
-print(latest_date_test)
-trending_test_yrs = latest_date_test - (latest_date_test - earliest_date_test)*.25
-print(trending_test_yrs)
-X = np.array((col_date_test, col_recent_test)).T
-x = [1,2,3]
-y = [4,5,6]
-z = zip(x,y)
-print(list(z))
-nbrs = NearestNeighbors(n_neighbors=5, algorithm='ball_tree').fit(X)
-distances, indices = nbrs.kneighbors([[2092, 49999]])
+col_recent_train = np.array(train.recent_views)
+col_date_train = np.array(train.date_posted)
+
+test_dates = parseDates(col_date_test)
+train_dates = parseDates(col_date_train)
+
+avg_recent_test = getAverageRecent(col_recent_test)
+print (avg_recent_test)
+avg_recent_train = getAverageRecent(col_recent_train)
+print (avg_recent_train)
+avg_dates_test = getAverageRecent(test_dates)
+print(avg_dates_test)
+avg_dates_train = getAverageRecent(train_dates)
+print(avg_dates_train)
+
+X = np.array((test_dates, col_recent_test)).T
+test_nbrs = NearestNeighbors(n_neighbors=100, algorithm='ball_tree').fit(X)
+distances, indices = test_nbrs.kneighbors([[2092, 49999]])
 print (indices)
+#print (distances)
+
+Y = np.array((train_dates, col_recent_train)).T
+train_nbrs = NearestNeighbors(n_neighbors=100, algorithm='ball_tree').fit(Y)
+distances, indices = train_nbrs.kneighbors([[2092, 49999]])
+print(indices)
